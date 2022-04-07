95435597286483b0fa316d1fc4f38d863780f72a
Nicholas Tillmann <nit36@pitt.edu>
2022-04-07 18:27:54 -0400

    Ajax RESTful APIs

---
+++ AjaxAPI/countris.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for, request, redirect
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("main-page.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ AjaxAPI/static/js/main-javascript.js
@@ -0,0 +1,40 @@
+// Global Variables
+var countriesListDropDown;
+function setup() {
+    countriesListDropDown = document.getElementById("countries");
+    countriesListDropDown.addEventListener("change", newCountrySelection);
+    fetchListOfCountries();
+}
+function fetchListOfCountries() {
+    // listOfCountries = ['Country 01', 'Country 02', 'Country 03'];
+    fetch('https://restcountries.com/v3.1/all').then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let listOfCountries = getListOfCountries(data)
+            initializeDropDown(listOfCountries)
+
+        })
+}
+function initializeDropDown(listOfCountries) {
+    let options = "";
+    for (let i = 0; i < listOfCountries.length; i++) {
+        options += `<option value="${listOfCountries[i]}">${listOfCountries[i]}</option>`;
+    }
+    countriesListDropDown.innerHTML = options;
+    countriesListDropDown.selectedIndex = Math.floor(Math.random() * listOfCountries.length);
+    displayCountryInfo(countriesListDropDown[countriesListDropDown.selectedIndex].value);
+}
+function newCountrySelection(event) {
+    displayCountryInfo(event.target.value);
+}
+function displayCountryInfo(countryName) {
+    document.getElementById("flag-container").src = "https://restcountries.com/data/png/col.png";
+    document.getElementById("flag-container").alt = 'Flag of Banana Republic';
+    document.getElementById("capital").innerHTML = 'Coconut';
+    document.getElementById("population").innerHTML = '123456';
+    document.getElementById("currencies").innerHTML = 'Dolar';
+    document.getElementById("region").innerHTML = 'Americas';
+    document.getElementById("subregion").innerHTML = 'Latin America';
+    document.getElementById("googleMap").href = "https://goo.gl/maps/uDWEUaXNcZTng1fP6";
+}
+window.addEventListener('load', setup);
