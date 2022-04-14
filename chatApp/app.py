3c3bb14210cb41cb165a3b77ca90a730b7d2668a
Nicholas Tillmann <nit36@pitt.edu>
2022-04-14 02:23:15 -0400

    begin working on final chat app for web dev class

--- chatApp/app.py
+++ chatApp/app.py
@@ -9,15 +9,40 @@ db = SQLAlchemy(app)
 
 class userChatter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
     username = db.Column(db.String(30), nullable=False)
-    content = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    retypepassword = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
 
 
 @app.route("/")
 def default():
     return render_template("index.html")
 
+
+# @app.route("/login/", methods=["GET", "POST"])
+# def login_controller():
+
+
+@app.route("/register/", methods=["GET", "POST"])
+def register_controller():
+    return render_template("register.html")
+
+
+# @app.route("/profile/<username>")
+# def profile(username=None):
+
+
+# @app.route("/logout/")
+# def unlogger():
+
+
+# @app.route("/new_message/", methods=["POST"])
+# def new_message():
+
+
+# @app.route("/messages/")
+# def messages():
 
 if __name__ == "__main__":
     app.run(debug=True)
