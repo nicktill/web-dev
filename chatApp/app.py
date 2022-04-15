b8086af3e44330e026e6569c685db49532218b5d
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-04-15 13:01:00 -0400

    adding app routes for chat app and get requests for register user

--- chatApp/app.py
+++ chatApp/app.py
@@ -9,10 +9,34 @@ db = SQLAlchemy(app)
 
 class userChatter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
     username = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(150), nullable=False)
     retypepassword = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), nullable=False)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+        username_ = request.form['username']
+        email_ = request.form['email']
+        password_ = request.form['password']
+        rePassword_ = request.form['repassword']
+        addUserInfo = userChatter(
+            username=(username_),
+            email=(email_),
+            password=(password_),
+            repassword=(rePassword_)
+        )
+        try:
+            db.session.add(addUserInfo)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your information to database'
+    else:
+        return render_template("index.html")
 
 
 @app.route("/")
